15e84b3d180f912a2b3f756ed1881940aa9be732
AyushRai7 <ayushrai1729@gmail.com>
2025-06-25 01:03:52 +0530

    update db url

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,5 +1,17 @@
+require('dotenv').config(); 
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:root@localhost:5432/task_manager');
+
+const sequelize = new Sequelize(process.env.DB_URL || 'postgres://postgres:root@localhost:5432/task_manager', {
+  dialect: 'postgres',
+  dialectOptions: process.env.DB_URL
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
+});
 
 const db = {};
 db.sequelize = sequelize;
